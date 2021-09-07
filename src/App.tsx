9b731b2540387d273f14c5d31ef15eedd088ae71
noe baiocchi <noe.baiocchi@epitech.eu>
2021-09-07 10:32:26 -0400

    push

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.css";
+import axios from 'axios'
+
+const App: React.FC = () => {
+  return (
+  <div className="App">
+    <span className="heading">React Meteo</span>
+  </div>
+  );
+};
+
+export default App;
---
+++ src/reducer/index.ts
@@ -0,0 +1,48 @@
+export const GET_WEATHER = 'GET_WEATHER';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_ERROR = 'SET_ERROR';
+export const SET_ALERT = 'SET_ALERT';
+
+export interface Weather {
+    description: string;
+    icon: string;
+    id: number;
+    main: string;
+}
+
+export interface WeatherData {
+    base : string;
+    clouds: {
+        all: number;
+    };
+    coord: {
+        lon: number;
+        lat: number;
+    };
+    cod: number;
+    dt: number;
+    id: number;
+    main: {
+        feels_like: number;
+        humidity: number;
+        pressure: number;
+        temp: number;
+        temp_max: number;
+        temp_min: number;
+    };
+    name: string;
+    sys: {
+        country: string;
+        id: number;
+        sunrise: number;
+        sunset: number;
+        type: number;
+    };
+    timezone: number;
+    visibility: number;
+    weather: Weather[];
+    wind: {
+        speed: number;
+        deg: number;
+    };
+}
