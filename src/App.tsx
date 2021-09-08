75e6588a55391f027984aca0d30f15e170674289
noe baiocchi <noe.baiocchi@epitech.eu>
2021-09-08 05:46:58 -0400

    add axios call api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
-import "./App.css";
+import React, { useState } from 'react'
+import './App.css'
 import axios from 'axios'
+import { Button } from "./components/button"
+import { AppContainer} from "./components/style";
 
-const App: React.FC = () => {
+function App() {
+  const [meteo1, setMeteo1] = useState("");
+  const [meteo2, setMeteo2] = useState("");
+  const [meteo3, setMeteo3] = useState("");
+
+  const meteolyon = () => {
+    axios.get("http://api.openweathermap.org/data/2.5/weather?q=Lyon&appid=8c2dbdca747ed150544b60e2ae60fbf1").then(
+      (response) => {
+        setMeteo1(response.data.weather[0].description);
+        setMeteo2(response.data.wind.speed);
+        setMeteo3(response.data.main.humidity);
+      }
+    );
+  };
+  
   return (
-  <div className="App">
-    <span className="heading">React Meteo</span>
-  </div>
-  );
-};
+  <AppContainer  style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+      <span className="heading3">React Meteo</span>
+      <Button onClick={meteolyon}>Check Meteo Lyon</Button>
+        <section className="section">
+              <div className="level-item has-text-centered">
+                <div>
+                <p className="heading">Weather description</p>
+                <p className="heading2">{meteo1}</p>
+                </div>
+              </div>
+              <div className="level-item has-text-centered">
+                <div>
+                  <p className="heading">humidity</p>
+                  <p className="heading2">{meteo3}</p>
+                </div>
+              </div>
+                <div>
+                  <p className="heading">wind</p>
+                  <p className="heading2">{meteo2}</p>
+              </div>
+        </section>
+    </AppContainer>
+    )
+}
 
-export default App;
+export default App
---
+++ src/components/style.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+    with: 100vw;
+    height: 100vh;
+    align-items: center;
+    background-color: lightblue;
+`;
